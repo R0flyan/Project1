4ba936ffddb564f87e37afcc35659f1e9dfb3aa2
R0flyan <max.sitosenko@gmail.com>
2023-10-19 12:58:12 +0300

    1commit

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", hideshowinfo());
+document.addEventListener("DOMContentLoaded", hideshowportf());
+document.getElementById('menu_btn').addEventListener("click", hideshowinfo);
+    function hideshowinfo() {
+        let x = document.getElementById('info');
+        if (x.style.display == "none"){
+        x.style.display = "block";
+        } else {
+        x.style.display = "none"}
+    };
+document.getElementById('portfolio_btn').addEventListener("click", hideshowportf);
+    function hideshowportf() {
+        let list = document.getElementById('portfolio_list1');
+        if (list.style.display == "none"){
+        list.style.display = "block";
+        } else {
+        list.style.display = "none"}
+    };
