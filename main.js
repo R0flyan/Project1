e0d34be5cd52ce1974575fec5795ee3c1c4148b1
R0flyan <max.sitosenko@gmail.com>
2023-10-20 21:03:04 +0300

    1.3commit

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
-document.addEventListener("DOMContentLoaded", hideshowinfo());
-document.addEventListener("DOMContentLoaded", hideshowportf());
+// document.addEventListener("DOMContentLoaded", hideshowinfo());
+// document.addEventListener("DOMContentLoaded", hideshowportf());
+// document.getElementById('menu_btn').addEventListener("click", hideshowinfo);
+//     function hideshowinfo() {
+//         let x = document.getElementById('info');
+//         if (x.style.display == "none"){
+//         x.style.display = "block";
+//         x.style.opacity = 1;
+//         } else {
+//         x.style.display = "none"}
+//     };
+// document.getElementById('portfolio_btn').addEventListener("click", hideshowportf);
+//     function hideshowportf() {
+//         let list = document.getElementById('portfolio_list1');
+//         if (list.style.display == "none"){
+//         list.style.display = "block";
+//         } else {
+//         list.style.display = "none"}
+//     };
 document.getElementById('menu_btn').addEventListener("click", hideshowinfo);
     function hideshowinfo() {
-        let x = document.getElementById('info');
-        if (x.style.display == "none"){
-        x.style.display = "block";
-        x.style.opacity = 1;
+        var x = document.querySelector('.info');
+        if (x.classList == "info"){
+        x.classList.add("info_active");
         } else {
-        x.style.display = "none"}
+        x.classList.remove("info_active")}
     };
 document.getElementById('portfolio_btn').addEventListener("click", hideshowportf);
     function hideshowportf() {
-        let list = document.getElementById('portfolio_list1');
-        if (list.style.display == "none"){
-        list.style.display = "block";
+        var list = document.querySelector('.portfolio_list');
+        if (list.classList == "portfolio_list"){
+        list.classList.add("portfolio_list_active");
         } else {
-        list.style.display = "none"}
+        list.classList.remove("portfolio_list_active")}
     };
